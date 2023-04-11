ab09fdf721398201f3886c98dc0d374484d85515
steven llaja <steven77dev@gmail.com>
2023-04-11 00:46:22 -0500

    avanace con eureka

--- msc-persona/src/main/java/com/restaurante/microservicios/mscpersona/MscPersonaApplication.java
+++ msc-persona/src/main/java/com/restaurante/microservicios/mscpersona/MscPersonaApplication.java
@@ -2,8 +2,10 @@ package com.restaurante.microservicios.mscpersona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MscPersonaApplication {
 
 	public static void main(String[] args) {
--- msc-personal/src/main/java/com/restaurante/microservicios/mscpersonal/MscPersonalApplication.java
+++ msc-personal/src/main/java/com/restaurante/microservicios/mscpersonal/MscPersonalApplication.java
@@ -2,12 +2,14 @@ package com.restaurante.microservicios.mscpersonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages  = { "com.restaurante.microservicios.mscpersonal","com.restaurante.microservicios.mscpersonal.mapper"})
 @EnableFeignClients
+@EnableEurekaClient
 public class MscPersonalApplication {
 
 	public static void main(String[] args) {
