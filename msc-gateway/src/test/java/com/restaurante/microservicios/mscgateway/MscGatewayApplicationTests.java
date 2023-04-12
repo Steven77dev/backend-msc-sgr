6555a817e8cbf7379afd7b0cd189962579d42192
steven llaja <steven77dev@gmail.com>
2023-04-12 01:48:59 -0500

    agregando gateway

---
+++ msc-gateway/src/test/java/com/restaurante/microservicios/mscgateway/MscGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.microservicios.mscgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
