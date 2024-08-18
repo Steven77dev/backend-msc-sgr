4c28b63d9f265d9a0b408da577fe44fafe2fecc4
steven llaja <steven77dev@gmail.com>
2024-08-17 22:08:40 -0500

    fix: correccion error msc finanza eureka

--- msc-finanzas/src/main/java/com/restaurante/microservicios/mscfinanzas/MscFinanzasApplication.java
+++ msc-finanzas/src/main/java/com/restaurante/microservicios/mscfinanzas/MscFinanzasApplication.java
@@ -2,13 +2,14 @@ package com.restaurante.microservicios.mscfinanzas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.restaurante.microservicios.mscfinanzas","com.restaurante.microservicios.common", "com.restaurante.microservicios.mscfinanzas.mapper"})
 @EnableFeignClients
-//@EnableEurekaClient
+@EnableEurekaClient
 public class MscFinanzasApplication {
 
     public static void main(String[] args) {
