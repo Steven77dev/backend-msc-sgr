6879e6f16f6dc7e88b5c17ac36d7d52c9925e285
steven llaja <steven77dev@gmail.com>
2024-08-18 13:40:48 -0500

    feat: probando puertos

--- msc-comercial/src/main/java/com/restaurante/microservicios/msccomercial/MscComercialApplication.java
+++ msc-comercial/src/main/java/com/restaurante/microservicios/msccomercial/MscComercialApplication.java
@@ -2,13 +2,14 @@ package com.restaurante.microservicios.msccomercial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.restaurante.microservicios.msccomercial","com.restaurante.microservicios.common"})
 @EnableFeignClients
-//@EnableEurekaClient
+@EnableEurekaClient
 public class MscComercialApplication {
 
     public static void main(String[] args) {
